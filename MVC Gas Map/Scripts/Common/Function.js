1645f867b23ee56a31c69cd4306128ed0ad5b89a
dquanghuy4444 <wepiohip@gmail.com>
2020-06-25 19:58:40 +0700

    25/6

--- MVC Gas Map/Scripts/Common/Function.js
+++ MVC Gas Map/Scripts/Common/Function.js
@@ -1,1 +1,65 @@
 ﻿
+
+    function message_Success(strMess) {
+        $('.notifyjs-wrapper').remove();
+    $.notify(
+        strMess,
+        "success"
+    );
+}
+
+        function scrollToSection(id) {
+            var targetElement = $('#' + id);
+            $([document.documentElement, document.body]).animate({
+        scrollTop: targetElement.offset().top
+}, 900);
+}
+
+        function checkValidTxt(idTxt) {
+            var strLength = "Nhập thiếu .Thông tin phải bằng hoặc lớn hơn 6 kí tự";
+    var strTxt = $('#' + idTxt).val();
+
+            if (!strTxt) {
+        message_Error(idTxt, STR_WARNING_EMPTY);
+    return false;
+}
+            else if (strTxt.length < 6) {
+        message_Error(idTxt, STR_WARNING_EMPTY);
+    return false;
+}
+return true;
+}
+
+//check caplock
+var flagCheckCapLock = true;
+var strCapLock = "Cap Lock đang bật.Hãy tắt để nhập thông tin";
+
+        function checkCapLock(idTxt) {
+            var txt = document.getElementById(idTxt);
+            txt.addEventListener("keyup", function (event) {
+                if (event.getModifierState("CapsLock")) {
+        flagCheckCapLock = false;
+    message_Error(idTxt, strCapLock);
+
+                } else {
+        flagCheckCapLock = true;
+    $('.notifyjs-wrapper').remove();
+}
+});
+}
+        function message_Error(id, strMess) {
+            if (id === '') {
+        //setTimeout($('.notifyjs-wrapper').remove(), 2000);
+        $.notify(
+            strMess,
+            { position: "top-center" }
+        );
+    }
+            else {
+        //setTimeout($('.notifyjs-wrapper').remove(), 2000);
+        $("#" + id).notify(
+            strMess,
+            { position: "top center" }
+        );
+    }
+}
--- MVC Gas Map/Scripts/Common/Variable.js
+++ MVC Gas Map/Scripts/Common/Variable.js
@@ -1,7 +1,24 @@
-﻿const STR_DEFAULT_NAME_GUEST = "Người dùng";
+﻿//------------------STRING
+const STR_DEFAULT_NAME_GUEST = "Người dùng";
 const STR_WARNING_EMPTY = "Chưa nhập thông tin";
 const STR_MESS_ERROR_LOAD_FAIL = "Truy cập dữ liệu thất bại";
 const STR_HREF_ERROR_404_PAGE = "/Error/PageNotFound";
 const STR_OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const STR_OSM_ATTRIBUTE = 'Có làm mới có ăn !!! ';
+
+
+
+//------------------INTEGER
 const INT_ZOOM_MAP_LEVEL = 15;
+
+
+//----------------------ARRAY
+const ARR_PAGE_GO_TO_404_PAGE =
+    [
+        "/Guest/Index",
+        "/Guest/Details",
+        "/Store/Details",
+        "/Store/Create",
+        "/User/Edit",
+    ];
+
